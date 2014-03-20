0a6fc3aa5d331121bc97ec78ece840a0e2e3c5bf
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2014-03-19 18:12:30 -0600

    package manifest

---
+++ pyvenny/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyvenny',
+      version='0.1',
+      description='Venn diagram plot of four sets, like venny.',
+      url='http://github.com/CSB-IG/pyvenny',
+      author='Rodrigo Garcia',
+      author_email='rgarcia@inmegen.gob.mx',
+      license='GPLv3',
+      packages=['pyvenny'],
+      zip_safe=False)
