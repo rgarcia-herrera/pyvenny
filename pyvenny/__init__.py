d70393c392dafcc94aafed4cbff760b8aadc27a6
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2014-03-19 20:19:17 -0600

    must group operation

--- pyvenny/__init__.py
+++ pyvenny/__init__.py
@@ -9,24 +9,24 @@ def fifteen_normalized_sections(a, b, c, d):
 
     # get sizes for regions delimited by set areas
     # _ is for - as in substract, i is for intersection, u is for union
-    return { 'A_BuCuD': "%0.2f" % len(a - set.union(b,c,d))/union,
-             'B_AuCuD': "%0.2f" % len(b - set.union(a,c,d))/union,
-             'C_AuBuD': "%0.2f" % len(c - set.union(a,b,d))/union,
-             'D_AuBuC': "%0.2f" % len(d - set.union(a,b,c))/union,
+    return { 'A_BuCuD': "%0.2f" % (len(a - set.union(b,c,d))/union),
+             'B_AuCuD': "%0.2f" % (len(b - set.union(a,c,d))/union),
+             'C_AuBuD': "%0.2f" % (len(c - set.union(a,b,d))/union),
+             'D_AuBuC': "%0.2f" % (len(d - set.union(a,b,c))/union),
 
-             'AiB_CuD': "%0.2f" % len(set.intersection(a,b) - set.union(c,d))/union,
-             'AiC_BuD': "%0.2f" % len(set.intersection(a,c) - set.union(b,d))/union,
-             'AiD_BuC': "%0.2f" % len(set.intersection(a,d) - set.union(b,c))/union,
-             'BiC_AuD': "%0.2f" % len(set.intersection(b,c) - set.union(a,d))/union,
-             'BiD_AuC': "%0.2f" % len(set.intersection(b,d) - set.union(a,c))/union,
-             'CiD_AuB': "%0.2f" % len(set.intersection(c,d) - set.union(a,b))/union,
+             'AiB_CuD': "%0.2f" % (len(set.intersection(a,b) - set.union(c,d))/union),
+             'AiC_BuD': "%0.2f" % (len(set.intersection(a,c) - set.union(b,d))/union),
+             'AiD_BuC': "%0.2f" % (len(set.intersection(a,d) - set.union(b,c))/union),
+             'BiC_AuD': "%0.2f" % (len(set.intersection(b,c) - set.union(a,d))/union),
+             'BiD_AuC': "%0.2f" % (len(set.intersection(b,d) - set.union(a,c))/union),
+             'CiD_AuB': "%0.2f" % (len(set.intersection(c,d) - set.union(a,b))/union),
 
-             'AiBiC_D': "%0.2f" % len(set.intersection(a,b,c) - d)/union,
-             'BiCiD_A': "%0.2f" % len(set.intersection(b,c,d) - a)/union,
-             'AiCiD_B': "%0.2f" % len(set.intersection(a,c,d) - b)/union,
-             'AiBiD_C': "%0.2f" % len(set.intersection(a,b,d) - c)/union,
+             'AiBiC_D': "%0.2f" % (len(set.intersection(a,b,c) - d)/union),
+             'BiCiD_A': "%0.2f" % (len(set.intersection(b,c,d) - a)/union),
+             'AiCiD_B': "%0.2f" % (len(set.intersection(a,c,d) - b)/union),
+             'AiBiD_C': "%0.2f" % (len(set.intersection(a,b,d) - c)/union),
 
-             'AiBiCiD': "%0.2f" % len(set.intersection(a,b,c,d))/union }
+             'AiBiCiD': "%0.2f" % (len(set.intersection(a,b,c,d))/union) }
 
 
 def render_four_set_venn( a, b, c, d, titles={'A': 'A',
